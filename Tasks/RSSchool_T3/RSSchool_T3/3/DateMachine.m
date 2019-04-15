72517003e17320b6d0314c4fafc4970833b97273
notayslo <antonsipaylo@gmail.com>
2019-04-15 23:59:32 +0300

    Completed

--- Tasks/RSSchool_T3/RSSchool_T3/3/DateMachine.m
+++ Tasks/RSSchool_T3/RSSchool_T3/3/DateMachine.m
@@ -32,6 +32,7 @@
   [self initStartDateTextField];
   [self initStepTextField];
   [self initDateUnitTextField];
+  self.startDateTextField.text = [self.dateFormatter stringFromDate:[self.dateFormatter dateFromString:self.currentDateLabel.text]];
 }
 
 - (void) initFormatters {
@@ -41,11 +42,13 @@
 
 - (void)initDateFormatter {
   self.dateFormatter = [NSDateFormatter new];
-  [self.dateFormatter setDateFormat:@"dd/MM/YYYY HH:mm:ss"];
+  [self.dateFormatter setDateFormat:@"dd/MM/yyyy HH:mm"];
+  NSLog(@"date format = %@", [self.dateFormatter dateFormat]);
 }
 
 - (void)initNumberFormatter {
   self.numberFormatter = [NSNumberFormatter new];
+  self.numberFormatter.locale = [NSLocale currentLocale];
   self.numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
 }
 
@@ -87,6 +90,7 @@
   NSString *normalTitle = @"add";
   self.addButton = [[UIButton alloc] init];
   [self initButton:self.addButton X:x Y:y Width:width Height:height TitleColor:[UIColor blackColor] Title:normalTitle];
+  self.addButton.accessibilityIdentifier = @"Add";
   [self addTargetToAddButton];
   [self.view addSubview:self.addButton];
 }
@@ -99,6 +103,7 @@
   NSString *normalTitle = @"sub";
   self.subButton = [[UIButton alloc] init];
   [self initButton:self.subButton X:x Y:y Width:width Height:height TitleColor:[UIColor blackColor] Title:normalTitle];
+  self.subButton.accessibilityIdentifier = @"Sub";
   [self.subButton setTitle:normalTitle forState:UIControlStateNormal];
   [self addTargetToSubButton];
   [self.view addSubview:self.subButton];
@@ -109,7 +114,9 @@
   self.startDate = [self checkStartDateTextField];
   self.dateUnit = [self checkDateUnitTextField];
   if (self.startDate == nil) {
+    NSLog(@"Start date is nil");
     self.startDate = [self.dateFormatter dateFromString:self.currentDateLabel.text];
+    NSLog(@"Start date = %@", [self.dateFormatter stringFromDate:self.startDate]);
   }
   return self.step != nil && self.dateUnit != nil;
 }
@@ -118,19 +125,33 @@
   NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
   NSInteger step = [self.step integerValue];
   if ([self.dateUnit isEqualToString:@"year"]) {
-    [dateComponents setYear:add? step : -step];
+    NSLog(@"1");
+    
+    dateComponents.year = (add? step : -step);
+//    [dateComponents setYear:(add? step : -step)];
   } else if ([self.dateUnit isEqualToString:@"month"]) {
-    [dateComponents setMonth:add? step : -step];
+    NSLog(@"2");
+    dateComponents.month = (add? step : -step);
+//    [dateComponents setMonth:(add? step : -step)];
   } else if ([self.dateUnit isEqualToString:@"week"]) {
-    [dateComponents setWeekday:add? step : -step];
+    NSLog(@"3");
+    dateComponents.day = 7 * (add? step : -step);
+//    [dateComponents setWeekday:(add? step : -step)];
   } else if ([self.dateUnit isEqualToString:@"day"]) {
-    [dateComponents setDay:add? step : -step];
+    NSLog(@"4");
+    dateComponents.day = (add? step : -step);
+//    [dateComponents setDay:(add? step : -step)];
   } else if ([self.dateUnit isEqualToString:@"hour"]) {
-    [dateComponents setHour:(add? step : -step)];
+    NSLog(@"5");
+    dateComponents.hour = (add? step : -step);
+//    [dateComponents setHour:(add? step : -step)];
   } if ([self.dateUnit isEqualToString:@"minute"]) {
-    [dateComponents setMinute:add? step : -step];
+    NSLog(@"6");
+    dateComponents.minute = (add? step : -step);
+//    [dateComponents setMinute:(add? step : -step)];
   }
   NSCalendar *calendar = [NSCalendar currentCalendar];
+  NSLog(@"Start date = %@", [self.dateFormatter stringFromDate:self.startDate]);
   NSDate *newDate = [calendar dateByAddingComponents:dateComponents toDate:self.startDate options:0];
   self.currentDateLabel.text = [self.dateFormatter stringFromDate:newDate];
   self.startDateTextField.text = [NSString stringWithString:[self.dateFormatter stringFromDate:newDate]];
@@ -165,6 +186,7 @@
   textField.layer.borderWidth = 1.f;
   textField.textColor = [UIColor blackColor];
   textField.textAlignment = NSTextAlignmentCenter;
+  textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
   textField.text = @"";
   textField.placeholder = placeholder;
   textField.delegate = self;
@@ -177,6 +199,7 @@
   CGFloat height = 50;
   NSString *placeholder = @"Start date";
   self.startDateTextField = [[UITextField alloc] init];
+  self.startDateTextField.accessibilityIdentifier = @"Start date";
   [self initTextField:self.startDateTextField X:x Y:y Width:width Height:height Placeholder:placeholder];
   [self.view addSubview:self.startDateTextField];
 }
@@ -189,6 +212,7 @@
   NSString *placeholder = @"Step";
   self.stepTextField = [[UITextField alloc] init];
   [self initTextField:self.stepTextField X:x Y:y Width:width Height:height Placeholder:placeholder];
+  self.stepTextField.accessibilityIdentifier = @"Step";
   [self.view addSubview:self.stepTextField];
 }
 
@@ -200,8 +224,39 @@
   NSString *placeholder = @"Date unit";
   self.dateUnitTextField = [[UITextField alloc] init];
   [self initTextField:self.dateUnitTextField X:x Y:y Width:width Height:height Placeholder:placeholder];
+  self.dateUnitTextField.accessibilityIdentifier = @"Date unit";
   [self.view addSubview:self.dateUnitTextField];
 }
+
+- (NSNumber*)checkStepTextField {
+  NSNumber *number = [self.numberFormatter numberFromString:self.stepTextField.text];
+  if (number == nil) {
+    self.stepTextField.textColor = [UIColor redColor];
+    self.stepTextField.text = @"Step";
+  }
+  return number;
+}
+
+- (NSDate*)checkStartDateTextField {
+  NSDate *date = [self.dateFormatter dateFromString:self.startDateTextField.text];
+  if (date == nil) {
+    self.startDateTextField.textColor = [UIColor redColor];
+    self.startDateTextField.text = @"Start date";
+  }
+  return date;
+}
+
+- (NSString*)checkDateUnitTextField {
+  if (![self.dateUnitValues containsObject:self.dateUnitTextField.text]) {
+    self.dateUnitTextField.textColor = [UIColor redColor];
+    self.dateUnitTextField.text = @"Date unit";
+    return nil;
+  }
+  return self.dateUnitTextField.text;
+}
+
+#pragma MARK UITextFieldDelegate
+
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
   textField.textColor = [UIColor blackColor];
@@ -218,35 +273,29 @@
   }
 }
 
-- (NSNumber*)checkStepTextField {
-  NSNumber *number = [self.numberFormatter numberFromString:self.stepTextField.text];
-  if (number == nil) {
-    self.stepTextField.textColor = [UIColor redColor];
-    self.stepTextField.text = @"Invalid step number";
-  }
-  return number;
-}
-
-- (NSDate*)checkStartDateTextField {
-  NSDate *date = [self.dateFormatter dateFromString:self.startDateTextField.text];
-  if (date == nil) {
-    self.startDateTextField.textColor = [UIColor redColor];
-    self.startDateTextField.text = @"Invalid date";
-  }
-  return date;
-}
-
-- (NSString*)checkDateUnitTextField {
-  if (![self.dateUnitValues containsObject:self.dateUnitTextField.text]) {
-    self.dateUnitTextField.textColor = [UIColor redColor];
-    self.dateUnitTextField.text = @"Invalid date unit";
-    return nil;
-  }
-  return self.dateUnitTextField.text;
-}
-
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
   return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string {
+  NSString *finalString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+  if (finalString.length == 0) return YES;
+  if (textField == self.stepTextField) {
+    return [self.numberFormatter numberFromString:finalString] != nil;
+  } else if (textField == self.startDateTextField) {
+    NSDate *newDate = [self.dateFormatter dateFromString:finalString];
+    if(newDate != nil) {
+      self.currentDateLabel.text = [self.dateFormatter stringFromDate:newDate];
+    }
+    return YES;
+  } else if (textField == self.dateUnitTextField) {
+    for (NSString *value in self.dateUnitValues) {
+      if([value hasPrefix:finalString]) return YES;
+    }
+  }
+  return NO;
 }
 
 @end
